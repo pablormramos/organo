430bb29a764b166f9d74f95ffcce64315de730ec
Pablo Ramos <pablo.ramos@interlandia.local>
2023-08-10 11:08:03 -0300

    first commit

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import Botao from '../Botao'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
+
+const Formulario = (props) => {
+    
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')        
+    }
+
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto 
+                    valor={nome} 
+                    aoAlterado={valor => setNome(valor)} 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu Nome" 
+                />
+                <CampoTexto 
+                    valor={cargo} 
+                    aoAlterado={valor => setCargo(valor)} 
+                    obrigatorio={true} label="Cargo" 
+                    placeholder="Digite seu Cargo" 
+                />
+                <CampoTexto 
+                    valor={imagem} 
+                    aoAlterado={valor => setImagem(valor)} 
+                    label="Imagem" 
+                    placeholder="Informe a URL da sua imagem" 
+                />
+                <ListaSuspensa
+                    obrigatorio={true} 
+                    label="Time" 
+                    itens={props.times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                />
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form>
+        </section>
+
+    )
+}
+
+export default Formulario
